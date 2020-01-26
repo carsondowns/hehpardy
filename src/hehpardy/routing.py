e1971d003bc39e096000969278c30c544236ffb8
Carson <acarsondowns@gmail.com>
2020-01-26 12:31:18 -0500

    Prior to redis step

--- src/hehpardy/routing.py
+++ src/hehpardy/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import pardy.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            pardy.routing.websocket_urlpatterns
+        )
+    ),
 })
--- src/pardy/views.py
+++ src/pardy/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'pardy/index.html', {})
+
+def room(request, room_name):
+    return render(request, 'pardy/room.html', {
+        'room_name': room_name
+    })
