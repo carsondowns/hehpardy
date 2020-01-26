7a2ce1084f911cf2fd972cd357850e98fd670098
Carson <acarsondowns@gmail.com>
2020-01-26 12:51:18 -0500

    Completed tutorial #1:
    https://www.youtube.com/watch?v=Wv5jlmJs2sU&list=PLLRM7ROnmA9EnQmnfTgUzCfzbbnc-oEbZ&index=1

--- src/hehpardy/settings.py
+++ src/hehpardy/settings.py
@@ -60,6 +60,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hehpardy.wsgi.application'
 ASGI_APPLICATION = 'hehpardy.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
